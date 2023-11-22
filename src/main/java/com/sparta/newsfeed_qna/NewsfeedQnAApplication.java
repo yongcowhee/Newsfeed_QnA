0c7fdbcfeb38e1e7aa9eb430b42649adc45c6a23
yongcowhee <yongcowhee@gmail.com>
2023-11-22 11:39:19 +0900

    Initial commit, Setting .gitignore

---
+++ src/main/java/com/sparta/newsfeed_qna/NewsfeedQnAApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.newsfeed_qna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsfeedQnAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsfeedQnAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/newsfeed_qna/NewsfeedQnAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.newsfeed_qna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsfeedQnAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
