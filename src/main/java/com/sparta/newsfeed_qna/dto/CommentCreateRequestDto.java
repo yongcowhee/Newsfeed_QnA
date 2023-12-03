8cff4f291f1860a0920a4365dcb80d79deafb370
yongcowhee <yongcowhee@gmail.com>
2023-12-03 17:54:29 +0900

    feat: CommentResponseDto 테스트 추가

--- src/main/java/com/sparta/newsfeed_qna/dto/CommentCreateRequestDto.java
+++ src/main/java/com/sparta/newsfeed_qna/dto/CommentCreateRequestDto.java
@@ -1,8 +1,11 @@
 package com.sparta.newsfeed_qna.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@AllArgsConstructor
 public class CommentCreateRequestDto {
     private String text;
 }
--- src/main/java/com/sparta/newsfeed_qna/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/newsfeed_qna/dto/CommentResponseDto.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Setter
 @Getter
 @NoArgsConstructor
 public class CommentResponseDto {
